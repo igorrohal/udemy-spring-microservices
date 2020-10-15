9b7e41a7b1dad35b3fdd63cdffe4b2946d491349
Igor Rohal <igor.rohal@gmail.com>
2020-10-15 13:13:36 +0200

    Eureka Naming Server introduced, with Ribbon taking a look in it and finding appropriate instances for load-balancing

--- spring-cloud/currency-conversion-service/src/main/java/com/irohal/springmicroservices/cloud/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ spring-cloud/currency-conversion-service/src/main/java/com/irohal/springmicroservices/cloud/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.irohal.springmicroservices.cloud.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.irohal.springmicroservices.cloud.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- spring-cloud/curreny-exchange-service/src/main/java/com/irohal/springmicroservices/cloud/currenyexchangeservice/CurrencyExchangeServiceApplication.java
+++ spring-cloud/curreny-exchange-service/src/main/java/com/irohal/springmicroservices/cloud/currenyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,12 +2,14 @@ package com.irohal.springmicroservices.cloud.currenyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class CurrenyExchangeServiceApplication {
+@EnableDiscoveryClient
+public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrenyExchangeServiceApplication.class, args);
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
 }
